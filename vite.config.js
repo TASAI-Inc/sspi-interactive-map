ec88e58f6295774cde7cfbb213369f43f73b3f04
Matthieu COLLÉ <druidev+github@gmail.com>
2024-04-15 22:08:29 -0400

    config: base relative path

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
