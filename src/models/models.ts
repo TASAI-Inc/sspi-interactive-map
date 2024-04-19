4252c2968997e29c6c8f7120d82a728105378416
Matthieu COLLÉ <druidev+github@gmail.com>
2024-04-19 08:51:49 -0400

    feat: added models

--- src/models/models.ts
+++ src/models/models.ts
@@ -6,3 +6,23 @@ export interface IIndicator {
 }
 
 export type Nullable<T> = T | null;
+
+export interface ICountryDataEntry {
+  name: string;
+  code: string;
+  scores: { [indicator: string]: number | null}
+}
+
+export interface IGeoJsonDataFeatures {
+  type: string;
+  properties: {
+    name: string;
+    cartodb_id: string;
+    scores: { [indicator: string]: number | null}
+  },
+  geometry: any;
+}
+export interface IGeoJsonData {
+  type: 'string',
+  features: IGeoJsonDataFeatures[];
+}
