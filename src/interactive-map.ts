c164161a1b4d5ff89f958363407557c8db65ee05
Matthieu COLLÉ <druidev+github@gmail.com>
2024-04-19 08:52:26 -0400

    feat(Indicators): on change callback options

--- src/interactive-map.ts
+++ src/interactive-map.ts
@@ -1,42 +1,67 @@
 import * as d3 from 'd3';
-import { IIndicator } from './models/models.ts';
 import { IndicatorFilter } from './indicator-filter.ts';
 
 export class InteractiveMap {
   private svg;
-  private geoGenerator: any;
-
+  private geoGenerator: any
   private indicatorFilter: IndicatorFilter;
-  private selectedIndicator: IIndicator;
+  private geoJsonData: any;
+
+  private readonly SVG_WIDTH: number = 600;
+  private readonly SVG_HEIGHT: number = 800;
+  private readonly MAP_COLORS: { [key: string]: string } = {
+    NO_RESPONSE: '#D1D3D4',
+    EXTREMELY_POOR: '#FF0000',
+    POOR: '#FFA700',
+    FAIR: '#FFF400',
+    GOOD: '#A3FF00',
+    EXCELLENT: '#2CBA00'
+  };
 
   constructor (svgElement: HTMLElement) {
     this.svg = d3.select(svgElement);
-    this.indicatorFilter = new IndicatorFilter();
+    this.indicatorFilter = new IndicatorFilter({
+      changeCallback: this.onChangeIndicator
+    });
   }
 
-  async generateMap (): Promise<void> {
-    const width: number = 600;
-    const height: number = 800;
-    let projection;
+  public async init (): Promise<void> {
+    this.geoJsonData = await this.loadGeoJson();
+    this.generateMap();
+  }
 
-    const data: any = await d3.json('scripts/data/africa.geojson');
+  private onChangeIndicator (): void {
 
-    projection = d3
+  }
+
+  private async loadGeoJson (): Promise<any> {
+    return d3.json('scripts/data/africa.geojson');
+  }
+
+  private getScaleColorFromValue (countryEntry: any): string {
+    console.log('entry',countryEntry)
+    return '';
+  }
+
+  private generateMap (): void {
+    let projection: d3.GeoProjection = d3
       .geoMercator()
-      .fitExtent([[0, 0], [width, height]], data);
+      .fitExtent([[0, 0], [this.SVG_WIDTH, this.SVG_HEIGHT]], this.geoJsonData);
     this.geoGenerator = d3.geoPath().projection(projection);
+
+    this.indicator = this.getSelectedIndicator();
 
     this.svg
       .append('g')
       .selectAll('path')
-      .data(data.features)
+      .data(this.geoJsonData.features)
       .enter()
       .append('g')
       .attr('class', function (d: any) {
         return d.properties.name;
       })
       .append('path')
-      .attr('fill', '#69b3a2')
+      .attr('fill', this.getScaleColorFromValue)
       .attr('d', this.geoGenerator)
       .style('stroke', '#fff')
       .on('mouseover', (e, d) => {
@@ -70,5 +95,9 @@ export class InteractiveMap {
       });
 
     d3.select(this).transition().duration(300).attr('fill', 'yellow');
+  }
+
+  private getSelectedIndicator (): string {
+    return this.indicatorFilter.selectedIndicator;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,5 @@ import { InteractiveMap } from './interactive-map.ts';
 
 window.addEventListener('DOMContentLoaded', async () => {
   const map: InteractiveMap = new InteractiveMap(document.getElementById('js-interactive-map') as HTMLElement);
-  await map.generateMap();
+  await map.init();
 });
