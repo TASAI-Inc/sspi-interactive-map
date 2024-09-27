46f555908092dd65ea46dbe59c10f49de09d3a07
camilogithub23 <camilo@camilographics.com>
2024-09-27 16:08:51 -0400

    cg09-27-2024 google analytics updates

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,6 +1,18 @@
 /*
 
-empty for now
-
+google event clicking the pdf
 
 */
+
+document.addEventListener('DOMContentLoaded', function() {
+    const pdfLink = document.querySelector('a[href="https://wp.tasai.org/wp-content/uploads/SSPI_report_2023_web.pdf"]'); // Replace with the actual PDF URL
+    if (pdfLink) {
+        pdfLink.addEventListener('click', function() {
+            // Send the event to Google Analytics 4
+            gtag('event', 'sspi_event_click', {
+                'sspi_pdf': pdfLink.href // Send the PDF URL as a parameter
+            });
+        });
+    }
+});
+
